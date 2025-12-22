2346c7aeeb2ea7a13c03abbefa8760ebf6659242
karanjaeats <karanjaeats0@gmail.com>
2025-12-23 01:10:09 +0530

    Add rate limiting middleware for API requests

---
+++ src/middlewares/rateLimit.middleware.js
@@ -0,0 +1,12 @@
+const rateLimit = require("express-rate-limit");
+
+module.exports = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // 100 requests per IP
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many requests, please try later"
+  }
+});
